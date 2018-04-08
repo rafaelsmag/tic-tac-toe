1779171490621c61c4906126af755b3e018597f5
Rafael S Magalhaes <rafaelsmagalhaes@Mac-mini-de-Rafael.local>
2018-04-08 16:34:34 -0300

    Starting repo

---
+++ src/modules/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../assets/img/logo.svg';
+import GameScreen from './game/containers/GameScreen'
+
+class App extends Component {
+  render() {
+    return (
+      <GameScreen />
+    );
+  }
+}
+
+export default App;
