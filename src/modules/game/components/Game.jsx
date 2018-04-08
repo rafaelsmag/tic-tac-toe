b879b05a87aa6c5d0d0578339d06dc3174e0faf5
Rafael S Magalhaes <rafaelsmag@gmail.com>
2018-04-08 17:36:26 -0300

    Adicionado board an renderização de jogadas

--- src/modules/game/components/Game.jsx
+++ src/modules/game/components/Game.jsx
@@ -2,11 +2,27 @@ import React, { Component } from 'react'
 import Board from './Board'
 
 class Game extends Component {
+  state = {
+    squares: Array(9).fill(null),
+    xIsNext: false
+  }
+
+  handleClick = (i) => {
+    const { xIsNext } = this.state
+    const squares = [...this.state.squares]
+    squares[i] = xIsNext ? 'X' : 'O'
+    this.setState({ squares, xIsNext: !xIsNext })
+  }
+
   render () {
+    const { squares } = this.state
     return (
       <div className="game">
         <div classNAme="game-board">
-          <Board />
+          <Board
+            squares={squares}
+            onClick={this.handleClick}
+          />
         </div>
       </div>
     )
--- src/modules/game/containers/GameScreen.jsx
+++ src/modules/game/containers/GameScreen.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Game from '../components/Game'
+import '../components/styles.css'
+
 class GameScreen extends React.Component {
   render () {
     return (
